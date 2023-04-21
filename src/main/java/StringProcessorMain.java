e324acc86fb0942ff31653e3472c4296915748de
Pablo Ortiz <paortiz@elpoli.edu.co>
2023-04-21 07:05:35 -0500

    added to execute printAllStrings with the following array of strings: String[] fourthSet = {"alpha", "beta", "gamma", "delta"};

--- src/main/java/StringProcessorMain.java
+++ src/main/java/StringProcessorMain.java
@@ -8,22 +8,23 @@ public class StringProcessorMain {
         int firstLength = 3;
         StringProcessor.printAllStrings(firstSet, firstLength);
 
-        System.out.println("First Test With Stack\n----------");
-        String[] firstSetWithStack = {"a", "b"};
-        int firstLengthWithStack = 3;
-        StringProcessorWithStack.printAllStringsWithStack(firstSetWithStack, firstLengthWithStack);
+        System.out.println("\nSecond Test\n----------");
+        String[] secondSet = {"c", "o", "d", "e"};
+        int secondLength = 2;
+        StringProcessor.printAllStrings(secondSet,
+                secondLength);
 
-//        System.out.println("\nSecond Test\n----------");
-//        String[] secondSet = {"c", "o", "d", "e"};
-//        int secondLength = 2;
-//        StringProcessor.printAllStrings(secondSet,
-//                secondLength);
-//
-//        System.out.println("\nThird Test\n----------");
-//        String[] thirdSet = {"c", "o", "d", "e"};
-//        int thirdLength = 5;
-//        StringProcessor.printAllStrings(thirdSet,
-//                thirdLength);
+        System.out.println("\nThird Test\n----------");
+        String[] thirdSet = {"c", "o", "d", "e"};
+        int thirdLength = 5;
+        StringProcessor.printAllStrings(thirdSet,
+                thirdLength);
+
+        System.out.println("\nFourth Test\n----------");
+        String[] fourthSet = {"alpha", "beta", "gamma", "delta"};
+        int fourthLength = 3;
+        StringProcessor.printAllStrings(fourthSet,
+                fourthLength);
     }
 
 }
